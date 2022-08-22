b200a08e49a99951bf0f64cf09ecc37732dbb6db
Sandeep Bhatia <sabhatia@gmail.com>
2022-08-21 17:13:35 -0700

    first commit

---
+++ bricks.py
@@ -0,0 +1,41 @@
+import pygame
+import sys
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+IS_GAME_OVER = False
+
+# Rectangle Parameters
+COLOR_RED = (255,0,0)
+RECT_POS = (400, 400)
+RECT_SIDE = 50
+RECT_SIZE = (RECT_SIDE, RECT_SIDE)
+
+PLAYER_RECTANGLE = pygame.Rect(RECT_POS, RECT_SIZE)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while not IS_GAME_OVER:
+    for game_event in pygame.event.get():
+
+        # Debugging only
+        print(game_event)
+
+        # Process key events
+        if game_event == pygame.QUIT:
+            sys.exit()
+
+        if game_event.type == pygame.KEYDOWN:
+            print("KEY DOWN")
+            if game_event.key == pygame.K_LEFT:
+                print("KEY_LEFT")
+            elif game_event.key == pygame.K_RIGHT:
+                print("KEY_RIGHT")
+
+    # Draw a rectangle
+    pygame.draw.rect(screen, COLOR_RED, PLAYER_RECTANGLE)
+
+    # Refresh the screen
+    pygame.display.update()
