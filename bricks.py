3df3d4ec4a82f32d3a1e0df5685f11288535e4c7
Sandeep Bhatia <sabhatia@gmail.com>
2022-09-03 16:58:37 -0700

    Add staggering to enemy ships

--- bricks.py
+++ bricks.py
@@ -1,5 +1,5 @@
 from cmath import rect
-from re import T
+from re import L, T
 import pygame
 import sys
 import random
@@ -27,18 +27,18 @@ RECT_INIT_X = RECT_INIT_Y = 400
 # Enemy Rectangle
 ENMY_RECT_INIT_X = random.randint(0, SCREEN_WIDTH - RECT_SIDE)
 ENMY_RECT_INIT_Y = 0
-ENMY_DROP_RATE = 25
+ENMY_DROP_RATE = 10
 ENMY_COUNT = 5
 
 # Player Rectangle
-rect_pos = (RECT_INIT_X, RECT_INIT_Y)
-x = rect_pos[0]
-y = rect_pos[1]
+player_ship = [RECT_INIT_X, RECT_INIT_Y]
+x = player_ship[0]
+y = player_ship[1]
 
 # Enemy Rectangle
-enmy_rect_pos = (ENMY_RECT_INIT_X, ENMY_RECT_INIT_Y)
-enmy_x = enmy_rect_pos[0]
-enmy_y = enmy_rect_pos[1]
+enemy_ship = [ENMY_RECT_INIT_X, ENMY_RECT_INIT_Y]
+enmy_x = enemy_ship[0]
+enmy_y = enemy_ship[1]
 enemies = list()
 
 # Clock
@@ -46,25 +46,22 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def generate_enemies(enemy_army: list, total_enemies: int):
-    for enemy in range(0, total_enemies):
+    debris_probability = random.random()
+    if (debris_probability > 0.1):
+        return
+
+    if len(enemy_army) < total_enemies:
         enemy_X = random.randint(0, SCREEN_HEIGHT - RECT_SIDE)
         enemy_Y = ENMY_RECT_INIT_Y
-        enemy_army.append((enemy_X, enemy_Y))
+        enemy_army.append([enemy_X, enemy_Y])
 
 def update_all_enemies(enemy_army):
-    for indx in range(0, len(enemy_army)):
-        enemy_army[indx] = update_enemy_pos(enemy_army[indx])
-
-def update_enemy_pos(enemy_block):
-    enemy_X = enemy_block[0]
-    enemy_Y = enemy_block[1]
-
-    enemy_Y += ENMY_DROP_RATE
-    if enemy_Y >= SCREEN_HEIGHT:
-        # move the enemy to a new horizontal position
-        enemy_X = random.randint(0, SCREEN_WIDTH - RECT_SIDE)
-        enemy_Y %= SCREEN_HEIGHT
-    return (enemy_X, enemy_Y)
+    for indx, enemy_ship in enumerate(enemy_army):
+        if enemy_ship[1] >= SCREEN_HEIGHT:
+            enemy_army.pop(indx)
+            print("Removed Enemy Ship[", indx, "]")
+        else:
+            enemy_ship[1] += ENMY_DROP_RATE
 
 def is_detected_collision_with_enemies(player, enemy_army):
     for enemy in enemy_army:
@@ -124,23 +121,23 @@ while not IS_GAME_OVER:
             # Compute the new rect co-ordinates
             x += x_delta
             y += y_delta
-            rect_pos = (x, y)
+            player_ship = [x, y]
 
     screen.fill((0,0,0))
 
     # Print debugs
-    print('Player:', rect_pos)
-    print('Enemy :', enmy_rect_pos)
+    print('Player:', player_ship)
+    print('Enemy :', enemy_ship)
 
     # Draw the rectangles
-    PLAYER_RECTANGLE = pygame.Rect(rect_pos, RECT_SIZE)
+    PLAYER_RECTANGLE = pygame.Rect(player_ship, RECT_SIZE)
     pygame.draw.rect(screen, COLOR_RED, PLAYER_RECTANGLE)
 
-    for enmy_rect_pos in enemies:
-        ENMY_RECTANGLE = pygame.Rect(enmy_rect_pos, RECT_SIZE)
+    for enemy_ship in enemies:
+        ENMY_RECTANGLE = pygame.Rect(enemy_ship, RECT_SIZE)
         pygame.draw.rect(screen, COLOR_BLUE, ENMY_RECTANGLE)
     
-    if is_detected_collision_with_enemies(rect_pos, enemies):
+    if is_detected_collision_with_enemies(player_ship, enemies):
         print("GAME OVER!")
         IS_GAME_OVER = True
 
@@ -150,6 +147,7 @@ while not IS_GAME_OVER:
 
     # Re-Compute Enemy rectangle
     update_all_enemies(enemies)
+    generate_enemies(enemies, ENMY_COUNT)
 
 #end while
 sys.exit()
